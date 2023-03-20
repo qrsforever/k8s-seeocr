223a370e597d5fc785b68c4e942f1ef14b408365
Li Dong <erlangai@qq.com>
2023-03-20 13:45:11 +0800

    save

--- app/test/kafka_producer.py
+++ app/test/kafka_producer.py
@@ -11,6 +11,7 @@ import time
 from kafka import KafkaProducer
 
 import socket
+import sys
 hostname = socket.gethostname()
 
 if hostname == 'storage':
@@ -23,29 +24,35 @@ producer = KafkaProducer(
         # key_serializer=str.encode,
         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-with open('./test.json', 'r') as fr:
-    value = json.load(fr)
 
-N = int(os.environ.get('N', 1))
+if __name__ == "__main__":
+    test_json = 'test.json'
+    if len(sys.argv) == 2:
+        test_json = sys.argv[1]
+        
+    with open(test_json, 'r') as fr:
+        value = json.load(fr)
 
-print('Count:', N)
+    N = int(os.environ.get('N', 1))
 
-try:
-    t0 = int(time.time())
-    for i in range(N):
-        token = int(time.time()) - t0
-        value[0]['cfg']['pigeon']['token'] = token
-        value[0]['cfg']['pigeon']['order'] = i
-        value[0]['cfg']['pigeon']['total'] = N
-        value[0]['cfg']['pigeon']['msgkey'] = 'seeocr_output'
-        future = producer.send(
-                'seeocr_input',
-                value=value[0])
-        time.sleep(2)
-        result = future.get(timeout=10)
-        print(f'1: {i} {token} {result}')
+    print('Count:', N)
 
-    producer.flush()
+    try:
+        t0 = int(time.time())
+        for i in range(N):
+            token = int(time.time()) - t0
+            value[0]['cfg']['pigeon']['token'] = token
+            value[0]['cfg']['pigeon']['order'] = i
+            value[0]['cfg']['pigeon']['total'] = N
+            value[0]['cfg']['pigeon']['msgkey'] = 'seeocr_output'
+            future = producer.send(
+                    'seeocr_input',
+                    value=value[0])
+            time.sleep(2)
+            result = future.get(timeout=10)
+            print(f'1: {i} {token} {result}')
 
-except Exception:
-    producer.close()
+        producer.flush()
+
+    except Exception:
+        producer.close()
